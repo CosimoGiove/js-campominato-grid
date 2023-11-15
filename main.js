8d20729e655b042e7751f24ff00460b257250952
cosimogiove <cosimo18497@gmail.com>
2023-11-15 15:46:20 +0100

    feat:controllo

--- main.js
+++ main.js
@@ -7,3 +7,26 @@ attraverso una funzione mi creo la mappa del gioco che deve essere 10 per 10.
 mi creo all'interno della mia funzione un elemento div, e lo appendo al mio file html.
 mi creo un elemento numero lo appendo al mio div
 attraverso un ciclo for gli assegno gli elmenti della funzione.*/
+
+const btn = document.querySelector(".btn")
+console.log(btn);
+
+
+btn.addEventListener("click", function () {
+    console.log("ciao")
+    const  NewGriglia = creazioneelemento("div","cella","containergioco")
+    console.log(NewGriglia)
+
+})
+
+function creazioneelemento(griglia,classname,contenuto) {
+    const elemento = document.createElement(griglia);
+    elemento
+
+
+    return elemento
+}
+
+
+
+
