660c73616f731f271af2cebd4c3461ba0bd094b0
cosimogiove <cosimo18497@gmail.com>
2023-11-15 14:49:08 +0100

    feat: scheletro del gioco

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 "use strict"
 
+/*# campo minato
+## esecuzione scritta
+attravero un imput mi creo un bottone e attraverso un evento click mi deve generare la mappa di campo minato.
+attraverso una funzione mi creo la mappa del gioco che deve essere 10 per 10.
+mi creo all'interno della mia funzione un elemento div, e lo appendo al mio file html.
+mi creo un elemento numero lo appendo al mio div
+attraverso un ciclo for gli assegno gli elmenti della funzione.*/
